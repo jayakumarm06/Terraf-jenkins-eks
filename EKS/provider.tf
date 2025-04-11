68dfc39a5a410fa6e6bd1fd443b17097d76263d5
jai <jaikumarm061286@gmail.com>
2025-04-11 12:40:26 +0530

    added files

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "jenkins-eks-bucket-02"
+    bucket = "jenkins-eks-bucket-03"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
 
--- jenkins-server/provider.tf
+++ jenkins-server/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "jenkins-eks-bucket-02"
+    bucket = "jenkins-eks-bucket-03"
     key    = "jenkins/terraform.tfstate"
     region = "us-east-1"
 
